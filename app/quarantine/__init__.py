bad7b11930f68026a38bca013c51f54e19de0814
Ash Wilson <ash.d.wilson@gmail.com>
2016-10-26 16:50:07 -0700

    Adjustments for Flake8

--- app/quarantine/__init__.py
+++ app/quarantine/__init__.py
@@ -1,4 +1,4 @@
-from config_helper import ConfigHelper
-from halo_general import HaloGeneral
-from halo_events import HaloEvents
-from matcher import Matcher
+from config_helper import ConfigHelper  # NOQA
+from halo_general import HaloGeneral  # NOQA
+from halo_events import HaloEvents  # NOQA
+from matcher import Matcher  # NOQA
