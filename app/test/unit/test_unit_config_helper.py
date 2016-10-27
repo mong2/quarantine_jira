e9b8c9093c48d509d0454e3765240eeddd8a85d0
Ash Wilson <ash.d.wilson@gmail.com>
2016-10-26 17:47:26 -0700

    Adding testing for ConfigHelper

--- app/test/unit/test_unit_config_helper.py
+++ app/test/unit/test_unit_config_helper.py
@@ -1,5 +1,6 @@
 import imp
 import os
+import re
 import sys
 
 module_name = 'quarantine'
@@ -12,8 +13,13 @@ fixture_path = os.path.join(here_dir, "../fixture/")
 
 
 class TestConfigHelper:
-    def test_get_good_match_list(self):
+    def test_get_match_list(self):
         control = ["first_event_type", "secondeventtype", "third_event_type"]
         good_file = os.path.join(fixture_path, "good_events")
         processed = quarantine.ConfigHelper.get_match_list(good_file)
         assert control == processed
+
+    def test_get_timestamp_inferred(self):
+        result = quarantine.ConfigHelper.get_timestamp()
+        print result
+        assert re.match('^\d{4}-\d{2}-\d{2}$', result)
