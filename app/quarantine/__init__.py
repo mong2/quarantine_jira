65af337ff77bbfca269ce00a532ed1dd526ae79e
Ash Wilson <ash.d.wilson@gmail.com>
2016-10-28 14:10:44 -0700

    Adding version and author info to __init__.py

--- app/quarantine/__init__.py
+++ app/quarantine/__init__.py
@@ -2,3 +2,7 @@ from config_helper import ConfigHelper  # NOQA
 from halo_general import HaloGeneral  # NOQA
 from halo_events import HaloEvents  # NOQA
 from matcher import Matcher  # NOQA
+
+__author__ = "CloudPassage"
+__version__ = "2.0"
+
