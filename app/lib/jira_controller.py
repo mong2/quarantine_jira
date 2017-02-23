8f4c71c7a871d4719c0705ad0419844b6249d1ce
Hana Lee <hlee@cloudpassage.com>
2017-02-23 01:27:26 -0800

    changed ticket format

--- app/lib/jira_controller.py
+++ app/lib/jira_controller.py
@@ -6,19 +6,27 @@ class JiraController(object):
     def __init__(self):
         self.config = CONFIG['jira']
         self.existing_tickets = {}
+        self.summary = ""
 
     def show_ticket(self, ticket_id):
         return JiraApi().get("/rest/api/2/issue/%s" % (ticket_id))
 
-    def form_ticket(self, event):
+    def form_key(self, event):
+        if event['type'] == 'vulnerable_software_package_found':
+            summary = "Halo Alert -- Vulnerable package:%s on %s" % (event['package_name'], event['server_hostname'])
+        else:
+            summary = "Halo Alert -- %s on %s" % (event['name'],event['server_hostname'])
+        return summary
+
+    def form_ticket(self, event, summary):
     	data = {
     		"fields":{
     			"project":
     			{
     				"key": self.config['project_key']
     			},
-    			"summary": "Halo Alert -- %s" % (event['name']),
-    			"description": "Creating this issue based on a triggered event in Halo\n Effected servers:\n%s" %(event['server_hostname']),
+    			"summary": summary,
+    			"description": "Creating this issue based on a triggered event in Halo\n {code}%s{code}" % (json.dumps(event, indent=2)),
     			"issuetype": {
     				"name": self.config['issue_name']
     			}
@@ -26,24 +34,12 @@ class JiraController(object):
     	}
     	return json.dumps(data,indent=2)
 
-    def form_description(self,event):
-        result = self.show_ticket(self.existing_tickets[event['type']])
-        update = "%s\n%s" % (result["fields"]["description"], event['server_hostname'])
-        words = update.split()
-        return " ".join(sorted(set(words), key=words.index))
-
-    def create_ticket(self, event):
-    	return JiraApi().post("/rest/api/2/issue/", self.form_ticket(event))
-
-    def update_ticket(self, event):
-        endpoint = "/rest/api/2/issue/%s" % self.existing_tickets[event['type']]
-        data = { "update":{ "description": [{"set":self.form_description(event)}]}}
-        return JiraApi().put(endpoint, json.dumps(data, indent=2))
+    def create_ticket(self, event, summary):
+    	return JiraApi().post("/rest/api/2/issue/", self.form_ticket(event, summary))
 
     def check_ticket_existence(self, event):
-        if event['type'] in self.existing_tickets.keys():
-            self.update_ticket(event)
-        else:
-            resp = self.create_ticket(event)
-            self.existing_tickets[event['type']] = resp["key"]
+        summary = self.form_key(event)
+        if summary not in self.existing_tickets.keys():
+            resp = self.create_ticket(event,summary)
+            self.existing_tickets[summary] = resp["key"]
             print self.existing_tickets
--- app/runner.py
+++ app/runner.py
@@ -16,12 +16,12 @@ matcher = quarantine.Matcher(config.match_list)
 jira = lib.JiraController()
 
 # Iterate over events, quarantine targeted workloads
-
 count = 0
 while count < 6:
     for event in events:
         if matcher.is_a_match(event["type"]):
+            # print event
             print "Quarantining workload: %s" % event["server_id"]
             jira.check_ticket_existence(event)
             count += 1
-            # halo.quarantine_workload(event["server_id"])
+#             # halo.quarantine_workload(event["server_id"])
