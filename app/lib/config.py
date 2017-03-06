208dcac650f1b6c5421803dd330d9a68782d4172
Hana Lee <hlee@cloudpassage.com>
2017-03-06 09:16:27 -0800

    moved config folder under app

--- app/lib/config.py
+++ app/lib/config.py
@@ -1,5 +1,5 @@
 import os
 import yaml
 
-PORTAL_CONFIG = os.path.join(os.path.dirname(__file__), '../../configs/configs.yml')
+PORTAL_CONFIG = os.path.join(os.path.dirname(__file__), '../configs/configs.yml')
 CONFIG = yaml.load(file(PORTAL_CONFIG, 'r'))
